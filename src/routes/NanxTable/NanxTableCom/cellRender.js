7889de376d391718afa18f88c3ddb0ccdc03174e
alex <tangyongjin@gmail.com>
2023-11-01 17:31:16 +0800

    简化逻辑

--- src/routes/NanxTable/NanxTableCom/cellRender.js
+++ src/routes/NanxTable/NanxTableCom/cellRender.js
@@ -6,25 +6,11 @@
 
 import CellsRenderHandlers from './cellRenders/cellsRenderHandlers';
 
-const CellRender = (text, record, column_cfg, datagrid_code, store) => {
-    if (text === '' || text === undefined) {
-        return '';
-    }
-    let field_cfg = {};
-    let table_columns_render_cfg = field_cfg[datagrid_code];
+const CellRender = (text, record, column_cfg, store) => {
     if (column_cfg.handler) {
         return CellsRenderHandlers[column_cfg.handler](text, record, store);
     }
-
-    // 不存在处理函数
-    if (table_columns_render_cfg === undefined) {
-        return text;
-    }
-
-    if (Object.keys(table_columns_render_cfg).includes(column_cfg.key) === false) {
-        return text;
-    }
-    return CellsRenderHandlers[table_columns_render_cfg[column_cfg.key]](text, record);
+    return text;
 };
 
 export default CellRender;
