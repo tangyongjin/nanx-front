178c8d43aff800c485640b2e5ffdf472e8c50049
alex <tangyongjin@gmail.com>
2023-11-25 00:30:27 +0800

    docu render download

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = {
     plugins: ['react-hot-loader/babel'],
     presets: [
         [
+            '@babel/preset-react',
+            {
+                throwIfNamespace: false // defaults to true
+            }
+        ],
+        [
             '@babel/preset-env',
             {
                 loose: true,
--- src/routes/NanxTable/NanxTableCom/cellRenders/OfficeDocuRender.js
+++ src/routes/NanxTable/NanxTableCom/cellRenders/OfficeDocuRender.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { root_url, port } from '@/api/api_config/base_config';
-
 const api_root = `${root_url}:${port}/`;
 function startsWithHttp(urlString) {
     return urlString.startsWith('http://');
@@ -10,21 +9,30 @@ const OfficeDocuRender = (url) => {
     if (url == '' || url == null || url == undefined) {
         return '';
     }
-    let _imgUrl = '';
+    let _download = '';
     if (startsWithHttp(url)) {
-        _imgUrl = url;
+        _download = url;
     } else {
-        _imgUrl = api_root + '/' + url;
+        _download = api_root + '/' + url;
     }
 
+    const segments = _download.split('/');
+
+    // 获取数组的最后一个元素
+    const lastSegment = segments[segments.length - 1];
+
+    const truncatedLastSegment =
+        lastSegment.length > 20 ? `${lastSegment.slice(0, 17)}...${lastSegment.slice(-3)}` : lastSegment;
+
     return (
-        <div>
-            <img
-                style={{ borderRadius: '5px', marginLeft: '30px', width: '30px', height: '30px' }}
-                alt="img"
-                src={_imgUrl}
-            />
-        </div>
+        <a href={_download}>
+            <div>
+                {truncatedLastSegment}
+                <svg style={{ fontSize: '24px', color: 'black' }} className="icon" aria-hidden="true">
+                    <use href="#icon-PDF"></use>
+                </svg>
+            </div>
+        </a>
     );
 };
 export default OfficeDocuRender;
