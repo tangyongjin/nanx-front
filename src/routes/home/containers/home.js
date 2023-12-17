0e925599871a7661d0c454fcd895ffede560b132
alex <tangyongjin@gmail.com>
2023-12-17 11:59:06 +0800

    动态按钮

--- src/routes/home/containers/home.js
+++ src/routes/home/containers/home.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Button } from 'antd';
-import IconWrapper from '@/utils/IconWrapper';
-import '@/UFormPlugin';
-import 'antd/dist/reset.css';
+import ButtonArray from './ButtonArray';
+import _NanxTableStore from '@/store/NanxTBS';
 
-const Home = () => {
-    const ix = IconWrapper('Bs:BsFileWord');
-    console.log('ix: ', ix);
+export default class NanxTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.tbStore = new _NanxTableStore();
+        console.log(this.tbStore);
+    }
 
-    return (
-        <div style={{ marginLeft: '40px' }}>
-            欢迎页面
-            <i className="bi-alarm"></i>
-            <h1>boost</h1>
-            <Button icon={ix} htmlType="button">
-                ABCD
-            </Button>
-        </div>
-    );
-};
+    render() {
+        const buttons = [
+            { buttontext: 'Button1', comPath: './a.js' },
+            { buttontext: 'Button2', comPath: './b.js' }
+        ];
 
-export default Home;
+        return (
+            <div>
+                <h1>Button Array</h1>
+                <ButtonArray tbStore={this.tbStore} buttons={buttons} />
+            </div>
+        );
+    }
+}
